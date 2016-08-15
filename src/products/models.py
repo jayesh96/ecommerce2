822104c8084a33b48527755e1172ca3cc0f9cc5e
Jayesh Bidani <jayesh.bidani@gmail.com>
2016-08-15 22:23:08 +0530

    Model Manager

--- src/products/models.py
+++ src/products/models.py
@@ -3,11 +3,24 @@ from django.db import models
 
 # Create your models here.
 
+class ProductQuerySet(models.query.QuerySet):
+	def active(self):
+		return self.filter(active=True)
+
+class ProductManager(models.Manager):
+	def get_queryset(self):
+		return ProductQuerySet(self.model, using=self._db)
+
+	def all(self, *args, **kwags):
+		return self.get_queryset().active()
+
 class Product(models.Model):
 	title = models.CharField(max_length=120)
 	description = models.TextField(blank=True, null=True)
 	price = models.DecimalField(decimal_places=2, max_digits=20)
 	active = models.BooleanField(default=True)
+
+	objects = ProductManager()
 
 	def __unicode__(self):
 		return self.title
