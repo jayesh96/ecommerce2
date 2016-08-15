4355ac9d8c3031eb5d44af15c71262de7c00f084
Jayesh Bidani <jayesh.bidani@gmail.com>
2016-08-15 11:22:48 +0530

    Add Html Template and List View

--- src/products/urls.py
+++ src/products/urls.py
@@ -4,12 +4,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 from .views import(
-    product_detail_view_func,
+    ProductListView,
     ProductDetailView
     )
 urlpatterns = [
     # Examples:
-    url(r'^cbv/(?P<pk>\d+)$', ProductDetailView.as_view(), name='product_detail'),
-    url(r'^(?P<id>\d+)$', product_detail_view_func, name='product_detail_function'),
+    url(r'^$', ProductListView.as_view(template_name="products/product_list.html"), name='products'),
+    url(r'^(?P<pk>\d+)$', ProductDetailView.as_view(template_name="products/product_detail.html"), name='product_detail'),
+    # url(r'^(?P<id>\d+)$', product_detail_view_func, name='product_detail_function'),
 
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -1,20 +1,26 @@
 from django.views.generic.detail import DetailView
-from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 from .models import Product
+
+
+class ProductListView(ListView):
+	model = Product
+
 
 class ProductDetailView(DetailView):
 	model = Product
 
 
-def product_detail_view_func(request, id):
-	product_instance = Product.objects.get(id=id)
-	template = "products/product_detail.html"
-	context={
-	"object":product_instance,
+# def product_detail_view_func(request, id):
+# 	# product_instance = Product.objects.get(id=id)
+# 	product_instance = get_object_or_404(Product, id=id)
+# 	template = "products/product_detail.html"
+# 	context={
+# 	"object":product_instance,
 
-	}
+# 	}
 
-	return render(request, template, context)
+# 	return render(request, template, context)
