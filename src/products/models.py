f0bab00bba72790d9b82322e099adaf0c50216f8
Jayesh Bidani <jayesh.bidani@gmail.com>
2016-08-16 15:14:17 +0530

    Product Variations

--- src/products/models.py
+++ src/products/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
 	title = models.CharField(max_length=120)
 	description = models.TextField(blank=True, null=True)
 	price = models.DecimalField(decimal_places=2, max_digits=20)
-	active = models.BooleanField(default=True)
+	active =  models.BooleanField(default=True)
 
 	objects = ProductManager()
 
@@ -27,3 +27,23 @@ class Product(models.Model):
 
 	def get_absolute_url(self):
 		return reverse("product_detail", kwargs={"pk": self.pk})
+
+class Variation(models.Model):
+	product = models.ForeignKey(Product)                                                           
+	title = models.CharField(max_length=120)
+	price = models.DecimalField(decimal_places=2, max_digits=20)
+	sale_price = models.DecimalField(decimal_places=2, max_digits=20, null=True, blank=True)
+	active = models.BooleanField(default=True)
+	inventory = models.IntegerField(default="-1", null=True, blank=True) #-1 refer unlimited amount
+
+	def __unicode__(self):
+		return self.title
+
+	def get_price(self):
+		if self.sale_price is not None:
+			return self.sale_price
+		else:
+			return self.price
+
+	def get_absolute_url(self):
+		return self.product.get_absolute_url()
