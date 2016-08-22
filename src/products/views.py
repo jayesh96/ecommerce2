997240ece277c586242a3440b9ed528d22b3e073
Jayesh Bidani <jayesh.bidani@gmail.com>
2016-08-22 23:21:33 +0530

    Dynamic Update Price with Jquery and Single Variation Price

--- src/products/views.py
+++ src/products/views.py
@@ -34,11 +34,10 @@ class VariationListView(StaffRequiredMixin, ListView):
 			formset.save(commit=False)
 			for form in formset:
 				new_item = form.save(commit=False)
-				if new_item.title:
-					product_pk = self.kwargs.get("pk")
-					product = get_object_or_404(Product, pk=product_pk)
-					new_item.product = product
-					new_item.save()
+				product_pk = self.kwargs.get("pk")
+				product = get_object_or_404(Product, pk=product_pk)
+				new_item.product = product
+				new_item.save()
 
 			messages.success(request, "Your Inventory is Updated")	
 			return redirect('products')	
